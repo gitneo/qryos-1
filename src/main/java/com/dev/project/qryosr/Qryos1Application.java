904b252f3f248a2fd8ae867890d18a202bebfb02
Ogo Okafor <ogookafor@gmail.com>
2025-06-16 23:53:24 +0200

    Initial commit

---
+++ src/main/java/com/dev/project/qryosr/Qryos1Application.java
@@ -0,0 +1,13 @@
+package com.dev.project.qryosr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qryos1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qryos1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/project/qryosr/Qryos1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.project.qryosr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qryos1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
