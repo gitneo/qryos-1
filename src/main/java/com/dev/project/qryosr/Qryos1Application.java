48ef58389536646172b0eff5473cca1ec436743d
Ogo Okafor <ogookafor@gmail.com>
2025-06-18 13:44:39 +0200

    add jenkins and Docker files

--- src/main/java/com/dev/project/qryosr/Qryos1Application.java
+++ src/main/java/com/dev/project/qryosr/Qryos1Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Qryos1Application {
 
     public static void main(String[] args) {
-        System.out.println("HELLO APP!");
         SpringApplication.run(Qryos1Application.class, args);
     }
 
